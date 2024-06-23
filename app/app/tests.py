c934a265cbc8d4effdf8dc99bd8edc57256ab03a
john77 <ionut_vasile85@hotmail.com>
2024-06-22 20:58:48 -0400

    adds first tests to the project

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers togheter."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test adding numbers togheter."""
+        res = calc.subtract(7, 11)
+
+        self.assertEqual(res, 4)
