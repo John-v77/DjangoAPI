44a6e1e928a73f2bdcc779c111d002bb909417e9
john77 <ionut_vasile85@hotmail.com>
2024-07-01 20:02:31 -0400

    fix user serializer error

--- app/user/views.py
+++ app/user/views.py
@@ -25,9 +25,9 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
-    serializers_class = UserSerializer
+    serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permissions_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
