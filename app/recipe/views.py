3ea8cdd979e8cf7b893caf2e8989a916d9d8d507
john77 <ionut_vasile85@hotmail.com>
2024-07-06 13:47:12 -0400

    Implement delete tag API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -40,6 +40,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 class TagViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet):
