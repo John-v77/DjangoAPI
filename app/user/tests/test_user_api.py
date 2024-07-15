4cd7ddd6ef8c397a5808480ad09f8f01fdd39909
john77 <ionut_vasile85@hotmail.com>
2024-07-14 20:00:21 -0400

    fixes upload recipe image API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -70,7 +70,7 @@ class PublicUserApiTests(TestCase):
     def test_create_token_for_user(self):
         """Test generates token for valid credentials"""
         user_details = {
-            'name': "Test Name",
+            'name': 'Test Name',
             'email': 'test@example.com',
             'password': 'test-user-password123'
         }
